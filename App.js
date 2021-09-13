a928cc5df4d4ccc9c4eded85ddd3421eb6f857d5
Rodolphe <dutchr@live.fr>
2021-09-13 13:37:30 +0200

    :sparkles: adding Input & Button Components

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
 import { StyleSheet, ScrollView, View } from "react-native";
 
-import Heading from "./components/Heading"
+import Heading from "./components/Heading";
+import Input from "./components/Input";
+import Button from "./components/Button";
+
+let todoIndex = 0;
 
 class App extends Component {
   constructor() {
@@ -12,16 +16,43 @@ class App extends Component {
       todos: [],
       type: "All",
     };
+    this.submitTodo = this.submitTodo.bind(this)
   }
+  inputChange(inputValue) {
+    console.log("Input value :", inputValue);
+    this.setState({ inputValue });
+  }
+  submitTodo() {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return;
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false,
+    };
+    todoIndex++;
+    const todos = [...this.state.todos, todo];
+    this.setState({ todos, inputValue: "" }, () => {
+      console.log("State: ", this.state);
+    });
+  }
+
   render() {
-    return(
-    <View style={styles.container}>
-      <ScrollView keyboardShouldPersistTaps='always' style={styles.content}>
-        <Heading />
-      </ScrollView>
-      <StatusBar style="auto" />
-    </View>
-    )
+    const { inputValue } = this.state;
+    return (
+      <View style={styles.container}>
+        <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
+          <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)}
+          />
+          <Button submitTodo={this.submitTodo} />
+        </ScrollView>
+        <StatusBar style="auto" />
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
@@ -31,8 +62,8 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    paddingTop: 60
-  }
+    paddingTop: 60,
+  },
 });
 
-export default App
+export default App;
