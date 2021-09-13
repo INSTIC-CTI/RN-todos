0709bcc8be19bc07c409499869d05a65d6ed1561
Rodolphe <dutchr@live.fr>
2021-09-13 11:33:22 +0200

    :sparkles: adding first component

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, ScrollView, View } from "react-native";
 
-export default function App() {
-  return (
+import Heading from "./components/Heading"
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: "",
+      todos: [],
+      type: "All",
+    };
+  }
+  render() {
+    return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView keyboardShouldPersistTaps='always' style={styles.content}>
+        <Heading />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
-  );
+    )
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f5f5",
   },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
 });
+
+export default App
